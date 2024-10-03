26baa6ea0195cab6bd7ee2cbcbd827f675b96d9b
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2024-10-03 00:04:02 -0300

    feat: init feedback area

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@ import Chat from "@/components/Chat";
 import Navbar from "@/components/Navbar";
 import HeaderHome from "@/components/HeaderHome";
 import Tools from "@/components/Tools";
+import Feedback from "@/components/Feedback";
 
 export default function Home(){
   return(
@@ -10,6 +11,7 @@ export default function Home(){
       <HeaderHome />
       <Chat />
       <Tools />
+      <Feedback />
     </main>
   )
 }
