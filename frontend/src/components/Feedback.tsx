0a541c175bd9538deac5e6b45af7f3b3f8f3c19f
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2024-10-08 18:05:48 -0300

    feat: init feedback area

--- frontend/src/components/Feedback.tsx
+++ frontend/src/components/Feedback.tsx
@@ -1,5 +1,21 @@
+import { FaChevronRight } from "react-icons/fa6";
+
 export default function Feedback(){
     return(
-        <section className="bg-feedback h-[120vh] w-full"></section>
+        <section className="bg-feedback h-[120vh] w-full flex flex-col items-center justify-center">
+            <h1 className="text-6xl font-semibold mb-10">Envie Seu Feedback</h1>
+            <div className="glass-white p-3 mb-10 ">
+                <p className="text-cloud text-xl w-[750px] text-center bg-biuld-with-ai p-10 rounded-md">Compartilhe suas ideias! Me diga quais 
+                    ferramentas você gostaria de ver no PlanejaAI para facilitar ainda mais o planejamento 
+                    das suas aulas e melhorar sua experiência.</p>
+            </div>
+            <div className="flex items-center gap-5">
+                <button className="bg-[#6138FB] text-white py-3 px-10 rounded-xl">Feedback</button>
+                <button className="flex items-center gap-3 text-white">
+                    <span>Github</span>
+                    <FaChevronRight />
+                </button>
+            </div>
+        </section>
     )
 }
