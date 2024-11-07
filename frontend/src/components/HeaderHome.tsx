38441ab954aebc192017ca80e9cb7f739167d4c8
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2024-11-06 23:51:12 -0300

    init: responsive header home

--- frontend/src/components/HeaderHome.tsx
+++ frontend/src/components/HeaderHome.tsx
@@ -2,19 +2,25 @@ import { FaChevronRight } from "react-icons/fa6";
 
 export default function NewHeaderHome(){
     return(
-        <section className="bg-header-home h-[150vh] w-full text-cloud">
-            <div className="pt-40 flex items-start justify-center">
-                <h1 className="text-6xl w-[800px] leading-[5rem] font-semibold">
+        <section className="bg-header-home h-[150vh] w-full text-cloud px-10">
+            <div className="pt-40 flex flex-col xl:flex-row items-start justify-center">
+                <h1 className="text-4xl text-start md:text-6xl w-full md:w-[600px] 
+                xl:w-[800px] md:leading-[5rem] font-semibold mb-10">
                     Planeje aulas rapidamente com o 
-                    <span className="bg-span px-5">PlanejaAI</span>
+                    <span className="bg-span px-8 py-1 md:px-5">PlanejaAI</span>
                 </h1>
-                <div className="w-[600px]">
+                <div className="w-full xl:w-[600px]">
                     <p className="text-xl mb-10">Precisa de ideias para suas aulas? Nosso Chat IA cria sugestões personalizadas para você em tempo real. 
                         Experimente agora e economize tempo no planejamento!</p>
                     <div className="flex items-center gap-5">
-                        <button className="bg-[#6138FB] text-white py-3 px-10 rounded-xl">Começar Agora</button>
-                        <button className="flex items-center gap-3 text-white">
+                        <button className="md:flex hidden bg-[#6138FB] text-white py-3 px-10 rounded-xl">Começar Agora</button>
+                        <button className="md:flex hidden items-center gap-3 text-white">
                             <span>Código no Github</span>
+                            <FaChevronRight />
+                        </button>
+                        <button className="flex md:hidden bg-[#6138FB] text-white py-3 px-10 rounded-xl">Começar</button>
+                        <button className="flex md:hidden items-center gap-3 text-white">
+                            <span>Github</span>
                             <FaChevronRight />
                         </button>
                     </div>
